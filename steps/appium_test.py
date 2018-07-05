6077a7e433887413af63fbd3d73c5a7942203f07
tasandra <tasandra40@gmail.com>
2018-07-04 18:50:21 -0700

    iphone test

--- steps/appium_test.py
+++ steps/appium_test.py
@@ -23,23 +23,23 @@ class Untitled(unittest.TestCase):
         self.wait = WebDriverWait(self.driver, 30)
 
     def testUntitled(self):
-        self.driver.execute_script("seetest:client.deviceAction('Home')")
-        self.driver.execute_script("seetest:client.deviceAction('Home')")
+        self.driver.find_element_by_id("Calendar").click()
+        self.driver.find_element_by_xpath("xpath=//*[@text='Saturday, July 7']").click()
         self.wait.until(
-            expected_conditions.presence_of_element_located((By.XPATH, "//*[@text='Touch ID or Enter Passcode']")))
-        self.driver.find_element_by_xpath("//*[@text='8']").click()
-        self.driver.find_element_by_xpath("//*[@text='6']").click()
-        self.driver.find_element_by_xpath("//*[@text='8']").click()
-        self.driver.find_element_by_xpath("//*[@text='9']").click()
-        self.driver.find_element_by_xpath("//*[@text='Calendar']").click()
-        self.driver.find_element_by_xpath("((//*[@class='UIAScrollView']/*[@class='UIAView'])[4]/*[@class='UIAButton'])[1]").click()
-        self.driver.find_element_by_xpath("(//*/*[@text and @class='UIAStaticText'])[2]").click()
+            expected_conditions.presence_of_element_located((By.XPATH, "//*[@text='Gelena']")))
+        event = self.driver.find_element_by_xpath("(//*/*[@text and @class='UIAStaticText'])[3]").text
+        self.assertEqual("Gelena", event, "Event title is not correct")
+
+        self.driver.find_element_by_id("Inbox").click()
         self.wait.until(
-            expected_conditions.presence_of_element_located((By.XPATH, "(//*/*[@class='UIAStaticText'])[1]")))
-        label = self.driver.find_element_by_xpath("(//*/*[@class='UIAStaticText'])[1]").text
-        self.assertEqual("Father's Day", label)
-        date = self.driver.find_element_by_xpath("(//*/*[@class='UIAStaticText'])[2]").text
-        self.assertEqual("Sunday, Jun 17, 2018", date)
+            expected_conditions.presence_of_element_located(
+                (By.XPATH, "(//*[@class='UIAStaticText']/*[@class='UIAButton'])[1]")))
+        new_button = self.driver.find_element_by_xpath("(//*[@class='UIAStaticText']/*[@class='UIAButton'])[1]").text
+
+        self.assertEqual("New", new_button, "New button is not present")
+
+        self.driver.find_element_by_xpath("//*[@text='Done']").click()
+        self.driver.execute_script("seetest:client.deviceAction(\"Home\")")
 
     def tearDown(self):
         self.driver.quit()
